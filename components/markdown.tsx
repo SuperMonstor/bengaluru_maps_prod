e8b7045503dd5371329b8c5d80ac3bba7dbfe664
Sudarshan S <senthilsudarshan@gmail.com>
2025-02-24 15:58:11 +0530

    markdown stuff fixed

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,6 +1,11 @@
 // components/markdown.tsx
 import ReactMarkdown from "react-markdown"
+import remarkGfm from "remark-gfm"
 
 export function Markdown({ content }: { content: string }) {
-	return <ReactMarkdown>{content}</ReactMarkdown>
+	return (
+		<div className="markdown-content prose prose-sm max-w-none">
+			<ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+		</div>
+	)
 }
