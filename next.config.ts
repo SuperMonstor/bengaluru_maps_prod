2e5a4ccd4b4686ac26ddf1d44970dc5211d69460
Sudarshan S <senthilsudarshan@gmail.com>
2025-02-25 16:37:52 +0530

    profile picture bug fix

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
 				protocol: "https",
 				hostname: "omnlyrfivyoazohpwdzu.supabase.co",
 			},
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+			},
 		],
 	},
 }
