4c8740e655503013f268a09fc53e4f601b479382
Sudarshan S <senthilsudarshan@gmail.com>
2025-03-03 17:27:49 +0530

    separate component for loading indicator

---
+++ components/custom-ui/loading-indicator.tsx
@@ -0,0 +1,7 @@
+export function LoadingIndicator() {
+  return (
+    <div className="min-h-[200px] border border-gray-300 rounded-md shadow-sm bg-white">
+      Loading editor...
+    </div>
+  )
+} 
